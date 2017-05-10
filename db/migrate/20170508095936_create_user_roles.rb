1f394a1a56a62ac238d7738f6124cd35d5bcd512
Blessings <blessings.mhango@gmail.com>
2017-05-10 11:29:31 +0200

    added nationality model

--- db/migrate/20170508095936_create_user_roles.rb
+++ db/migrate/20170508095936_create_user_roles.rb
@@ -2,7 +2,7 @@ class CreateUserRoles < ActiveRecord::Migration
   def change
     create_table :user_roles do |t|
       t.string :level
-
+      t.string :role
       t.timestamps null: false
     end
   end
