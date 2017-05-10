0fa068f4c86c392516c41e3cfe6eb7b4f2b9c589
Blessings <blessings.mhango@gmail.com>
2017-05-10 09:24:39 +0200

    added created site

--- db/migrate/20170508081946_create_people.rb
+++ db/migrate/20170508081946_create_people.rb
@@ -6,7 +6,9 @@ class CreatePeople < ActiveRecord::Migration
       t.string :middle_name
       t.string :last_name, null: false
       t.string :gender, null: false
-      t.datetime :birthdate, null: false 
+      t.datetime :birthdate, null: false
+      t.integer :creator, null: false
+      t.string :site_created, null: false 
       t.boolean :birthdate_estimated, null: false, default: 0
 
       t.timestamps null: false
