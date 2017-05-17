63dc882d60a3ccdda63439c45faf637c729da350
Blessings <blessings.mhango@gmail.com>
2017-05-17 09:45:52 +0200

    removed all migrations

--- db/migrate/20170508095936_create_user_roles.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserRoles < ActiveRecord::Migration
-  def change
-    create_table :user_roles do |t|
-      t.string :level
-      t.string :role
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20170508100056_create_role_activities.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoleActivities < ActiveRecord::Migration
-  def change
-    create_table :role_activities do |t|
-      t.string :activity
-
-      t.timestamps null: false
-    end
-  end
-end
