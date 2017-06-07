a0ae4687c479c47e4ed43c21769d9c075144b148
Blessings <blessings.mhango@gmail.com>
2017-06-07 11:38:34 +0200

    added model associations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,5 @@
 class City < ActiveRecord::Base
     self.table_name = :city
     self.primary_key = :city_id
+    belongs_to :country, foreign_key: "country_id"
 end
--- app/models/district.rb
+++ app/models/district.rb
@@ -1,4 +1,6 @@
 class District < ActiveRecord::Base
     self.table_name = :district
     self.primary_key = :district_id
+    belongs_to :region, foreign_key: "region_id"
+    has_many :traditional_authorities, foreign_key: "district_id"
 end
