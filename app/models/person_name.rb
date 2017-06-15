a129e802010860f033337ea56a0217e86dc08544
mwatha <mwathabwanali@gmail.com>
2017-06-16 00:43:39 +0200

    Now we can edit user details

--- app/models/person_name.rb
+++ app/models/person_name.rb
@@ -2,6 +2,7 @@ class PersonName < ActiveRecord::Base
     self.table_name = :person_name
     self.primary_key = :person_name_id
     include EbrsAttribute
+    default_scope { where(voided: 0) }
 
     belongs_to :person
     belongs_to :core_person
