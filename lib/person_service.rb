4146ae6822137308a3424e1b18cf56224ebbcc5a
Pachawo Bisani <phbisan@gmail.com>
2017-06-20 07:06:26 +0200

    Record creation at DC level

--- lib/person_service.rb
+++ lib/person_service.rb
@@ -5,8 +5,8 @@ module PersonService
   def self.create_record(params)
     #raise params.inspect
 
-    adoption_court_order              = params[:child][:adoption_court_order]
-    adoption_court_order              = params[:child][:informant][:designation]
+    adoption_court_order              = params[:person][:adoption_court_order] rescue nil
+    desig              = params[:person][:informant][:designation] rescue nil
     birth_place_details_available     = params[:birth_place_details_available]
     parents_details_available         = params[:parents_details_available]
     biological_parents                = params[:biological_parents]
@@ -21,67 +21,74 @@ module PersonService
     birth_weight		                  = params[:person][:birth_weight]
     acknowledgement_of_receipt_date	  = params[:person][:acknowledgement_of_receipt_date]
 
-    gender 				      	            = params[:child][:gender]
-    home_address_same_as_physical     = params[:child][:home_address_same_as_physical]
-    same_address_with_mother	        = params[:child][:same_address_with_mother]
-    registration_type 	              = params[:child][:registration_type]
-    copy_mother_name                  = params[:child][:copy_mother_name]
-    type_of_birth		                  = params[:child][:type_of_birth]
-    mother_last_name 	                =	params[:child][:mother][:last_name]
-    mother_first_name	                =	params[:child][:mother][:first_name]
-    mother_middle_name	              =	params[:child][:mother][:middle_name]
-    mother_birthdate	                =	params[:child][:mother][:birthdate]
-    mother_citizenship	              =	params[:child][:mother][:citizenship]
-    mother_residental_country         = params[:child][:mother][:residential_country]
-    mother_foreigner_current_district = params[:child][:mother][:foreigner_current_district]
-    mother_foreigner_current_village  = params[:child][:mother][:foreigner_current_village]
-    mother_foreigner_current_ta       = params[:child][:mother][:foreigner_current_ta]
-    mother_home_country               = params[:child][:mother][:home_country]
-    mother_foreigner_home_district    = params[:child][:mother][:foreigner_home_district]
-    mother_foreigner_home_village     = params[:child][:mother][:foreigner_home_village]
-    mother_foreigner_home_ta          = params[:child][:mother][:foreigner_home_ta]
-    mother_estimated_dob	            =	params[:child][:mother][:birthdate_estimated]
-    mother_mode_of_delivery           = params[:child][:mode_of_delivery]
-    mother_level_of_education         = params[:child][:level_of_education]
+    gender 				      	            = params[:person][:gender]
+    home_address_same_as_physical     = params[:person][:home_address_same_as_physical]
+    same_address_with_mother	        = params[:person][:same_address_with_mother]
+    registration_type 	              = params[:person][:registration_type]
+    copy_mother_name                  = params[:person][:copy_mother_name]
+    type_of_birth		                  = params[:person][:type_of_birth]
+
+    ################################ mother details ###############################################
+
+      mother_last_name 	                =	params[:person][:mother][:last_name]
+      mother_first_name	                =	params[:person][:mother][:first_name]
+      mother_middle_name	              =	params[:person][:mother][:middle_name]
+      mother_birthdate	                =	params[:person][:mother][:birthdate]
+      mother_citizenship	              =	params[:person][:mother][:citizenship]
+      mother_residental_country         = params[:person][:mother][:residential_country]
+      mother_foreigner_current_district = params[:person][:mother][:foreigner_current_district]
+      mother_foreigner_current_village  = params[:person][:mother][:foreigner_current_village]
+      mother_foreigner_current_ta       = params[:person][:mother][:foreigner_current_ta]
+      mother_home_country               = params[:person][:mother][:home_country]
+      mother_foreigner_home_district    = params[:person][:mother][:foreigner_home_district]
+      mother_foreigner_home_village     = params[:person][:mother][:foreigner_home_village]
+      mother_foreigner_home_ta          = params[:person][:mother][:foreigner_home_ta]
+      mother_estimated_dob	            =	params[:person][:mother][:birthdate_estimated]
+
+    mother_mode_of_delivery           = params[:person][:mode_of_delivery]
+    mother_level_of_education         = params[:person][:level_of_education]
+
+    ################################ mother details (end) #######################################
 
     ########################### father details ########################################
 
-    father_birthdate_estimated        = params[:child][:father][:birthdate_estimated]
-    father_residential_country        = params[:child][:father][:residential_country]
-    father_lastname                   = params[:child][:father][:last_name]
-    father_firstname                  = params[:child][:father][:first_name]
-    father_middlename                 = params[:child][:father][:middle_name]
-    father_birthdate                  = params[:child][:father][:birthdate]
-    father_citizenship                = params[:child][:father][:citizenship]
-    father_current_district           = params[:child][:father][:current_district]
-    father_current_ta                 = params[:child][:father][:current_ta]
-    father_current_village            = params[:child][:father][:current_village]
-    father_home_district              = params[:child][:father][:home_district]
-    father_home_ta                    = params[:child][:father][:home_ta]
-    father_home_village               = params[:child][:father][:home_village]
+      father_birthdate_estimated        = params[:person][:father][:birthdate_estimated]
+      father_residential_country        = params[:person][:father][:residential_country]
+      father_lastname                   = params[:person][:father][:last_name]
+      father_firstname                  = params[:person][:father][:first_name]
+      father_middlename                 = params[:person][:father][:middle_name]
+      father_birthdate                  = params[:person][:father][:birthdate]
+      father_citizenship                = params[:person][:father][:citizenship]
+      father_current_district           = params[:person][:father][:current_district]
+      father_current_ta                 = params[:person][:father][:current_ta]
+      father_current_village            = params[:person][:father][:current_village]
+      father_home_district              = params[:person][:father][:home_district]
+      father_home_ta                    = params[:person][:father][:home_ta]
+      father_home_village               = params[:person][:father][:home_village]
+
 
     ######################### father details (end) #################################
 
-    informant_last_name               = params[:child][:informant][:last_name]
-    informant_first_name              = params[:child][:informant][:first_name]
-    informant_middle_name             = params[:child][:informant][:middle_name]
-    informant_relationship_to_child   = params[:child][:informant][:relationship_to_child]
-    informant_current_district        = params[:child][:informant][:current_district]
-    informant_current_ta              = params[:child][:informant][:current_ta]
-    informant_current_village         = params[:child][:informant][:current_village]
-    informant_addressline1            = params[:child][:informant][:addressline1]
-    informant_addressline2            = params[:child][:informant][:addressline2]
-    informant_phone_number            = params[:child][:informant][:phone_number]
-    informant_form_signed             = params[:child][:form_signed]
-    informant_same_as_mother          = params[:child][:informant][:informant_same_as_mother]
-    informant_same_as_father          = params[:child][:informant][:informant_same_as_father]
+    informant_last_name               = params[:person][:informant][:last_name]
+    informant_first_name              = params[:person][:informant][:first_name]
+    informant_middle_name             = params[:person][:informant][:middle_name]
+    informant_relationship_to_child   = params[:person][:informant][:relationship_to_child]
+    informant_current_district        = params[:person][:informant][:current_district]
+    informant_current_ta              = params[:person][:informant][:current_ta]
+    informant_current_village         = params[:person][:informant][:current_village]
+    informant_addressline1            = params[:person][:informant][:addressline1]
+    informant_addressline2            = params[:person][:informant][:addressline2]
+    informant_phone_number            = params[:person][:informant][:phone_number]
+    informant_form_signed             = params[:person][:form_signed]
+    informant_same_as_mother          = params[:person][:informant][:informant_same_as_mother]
+    informant_same_as_father          = params[:person][:informant][:informant_same_as_father]
 
 
 
-    court_order_attached	            =	params[:child][:court_order_attached]
-    parents_signed                    = params[:child][:parents_signed]
+    court_order_attached	            =	params[:person][:court_order_attached]
+    parents_signed                    = params[:person][:parents_signed]
 
-    parents_married_to_each_other	    =	params[:child][:parents_married_to_each_other]
+    parents_married_to_each_other	    =	params[:person][:parents_married_to_each_other]
 
     month_prenatal_care_started               = params[:month_prenatal_care_started]
     number_of_prenatal_visits                 = params[:number_of_prenatal_visits]
@@ -129,27 +136,32 @@ module PersonService
     )
 
     ################################### recording mother details (start) ###############################################
-    core_person_mother = CorePerson.create(person_type_id: PersonType.where(name: 'Mother').first.id)
 
-    person_mother = Person.create(person_id: core_person_mother.id,
-        gender: "F",
-        birthdate: (mother_birthdate.to_date rescue Date.today))
+    if (parents_details_available == "Both" || parents_details_available == "Mother")
 
-    person_name_mother = PersonName.create(first_name: mother_first_name,
-        middle_name: mother_middle_name,
-        last_name: mother_last_name, person_id: core_person_mother.id)
+      core_person_mother = CorePerson.create(person_type_id: PersonType.where(name: 'Mother').first.id)
 
-    PersonNameCode.create(person_name_id: person_name_mother.id,
-        first_name_code: mother_first_name.soundex,
-        last_name_code: mother_last_name.soundex,
-        middle_name_code: (mother_middle_name.soundex rescue nil))
+      person_mother = Person.create(person_id: core_person_mother.id,
+          gender: "F",
+          birthdate: (mother_birthdate.to_date rescue Date.today))
 
-    PersonRelationship.create(person_a: core_person.id, person_b: core_person_mother.id,
-        person_relationship_type_id: PersonRelationType.where(name: 'Child-Mother').first.id)
+      person_name_mother = PersonName.create(first_name: mother_first_name,
+          middle_name: mother_middle_name,
+          last_name: mother_last_name, person_id: core_person_mother.id)
+
+      PersonNameCode.create(person_name_id: person_name_mother.id,
+          first_name_code: mother_first_name.soundex,
+          last_name_code: mother_last_name.soundex,
+          middle_name_code: (mother_middle_name.soundex rescue nil))
+
+      PersonRelationship.create(person_a: core_person.id, person_b: core_person_mother.id,
+          person_relationship_type_id: PersonRelationType.where(name: 'Child-Mother').first.id)
+    end
+
     ################################### recording mother details (end)   ###############################################
 
     ################################### recording father details (start) ###############################################
-    if(details_of_father_known == "Yes")
+    if(details_of_father_known == "Yes" || parents_details_available == "Both" || parents_details_available == "Father")
 
       core_person_father = CorePerson.create(person_type_id: PersonType.where(name: 'Father').first.id)
 
@@ -171,6 +183,7 @@ module PersonService
 
     end
     ################################### recording father details (end)   ###############################################
+    
     ################################### recording informant details (start) ############################################
     
     if (informant_same_as_mother == "Yes")
