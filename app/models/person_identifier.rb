d3cab0d93d2818f2ba3db7cfd28f0a84cb3cb07c
cranberry <manzymkamba@gmail.com>
2017-07-25 19:25:49 +0200

    Person identifier model changed

--- app/models/person_identifier.rb
+++ app/models/person_identifier.rb
@@ -2,5 +2,5 @@ class PersonIdentifier < ActiveRecord::Base
     self.table_name = :person_identifiers
     self.primary_key = :person_identifier_id
     belongs_to :core_person, foreign_key: "person_id"
-    belongs_to :person_identifier_type, foreign_key: "person_identifier_type_id"
+    belongs_to :person_identifier_types, foreign_key: "person_identifier_type_id"
 end
