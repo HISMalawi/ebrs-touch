3f8d3f240c6eefad30048d4e25f088533b40c5de
phundi <kennethkapundi1@gmail.com>
2017-08-09 23:20:07 +0200

    saving records using special lib, modular approach

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,4 +5,54 @@ class Person < ActiveRecord::Base
     has_many :person_names
     include EbrsAttribute
 
+    def mother
+      result = nil
+      relationship_type = PersonRelationType.find_by_name("Mother")
+
+      relationship = PersonRelationship.where(:person_a => self.person_id, :person_relationship_type_id => relationship_type.id).last
+      unless relationship.blank?
+        result = Person.where(:person_id => relationship.person_b).last
+      end
+
+      result
+    end
+
+    def father
+      result = nil
+      relationship_type = PersonRelationType.find_by_name("Father")
+
+      relationship = PersonRelationship.where(:person_a => self.person_id, :person_relationship_type_id => relationship_type.id).last
+      unless relationship.blank?
+        result = Person.where(:person_id => relationship.person_b).last
+      end
+
+      result
+    end
+
+    def informant
+      result = nil
+      relationship_type = PersonRelationType.find_by_name("Informant")
+
+      relationship = PersonRelationship.where(:person_a => self.person_id, :person_relationship_type_id => relationship_type.id).last
+      unless relationship.blank?
+        result = Person.where(:person_id => relationship.person_b).last
+      end
+
+      result
+    end
+
+    def citizenship
+      adr = PersonAddress.where(person_id: self.id).last
+      loc_name = Location.find(adr.citizenship).country  rescue nil
+      loc_name
+    end
+
+    def name
+      name = self.person_names.last
+      "#{name.first_name} #{name.middle_name} #{name.last_name}".gsub(/\s+/, ' ')
+    end
+
+    def full_gender
+      {'M' => 'Male', 'F' => 'Female'}[self.gender]
+    end
 end
