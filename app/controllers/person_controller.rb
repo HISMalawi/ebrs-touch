907ec66f5d7a19db8b264abbfac34b30d0a59bc4
phundi <kennethkapundi1@gmail.com>
2017-08-17 09:48:52 +0200

    Incomplee to Mark Pending

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -302,7 +302,6 @@ class PersonController < ApplicationController
   end
 
   def create
-      raise params.inspect
     type_of_birth = params[:person][:type_of_birth]
     
      if type_of_birth == 'Twin'
