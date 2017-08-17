186b315d924f747c50c998febcc0cf66d53ab59e
phundi <kennethkapundi1@gmail.com>
2017-08-17 08:08:02 +0200

    Fixed a big in sync filters

--- db/filters.js
+++ db/filters.js
@@ -1,6 +1,6 @@
 {
   "filters" :
     {
-        "my_location" : "function(doc, req){ var k = ''; if(doc['person_name_id']){k = doc['person_name_id'].toString()}; if(doc['person_id']){k = doc['person_id'].toString()}; if(doc['user_id']){k = doc['user_id'].toString()}; k = k.substr(0, 6); if(k == req.query.location_id){ return true }else{ return false }}"
+        "my_location" : "function(doc, req){ var k = ''; if(doc['person_a']){k = doc['person_relationship_id'].toString()}; if(doc['person_name_id']){k = doc['person_name_id'].toString()}; if(doc['person_id']){k = doc['person_id'].toString()}; if(doc['user_id']){k = doc['user_id'].toString()}; k = k.substr(0, 6); if(k == req.query.location_id){ return true }else{ return false }}"
     }
 }
