cb18bf714843f7f80cddc118780fb91090913c84
phundi <kennethkapundi1@gmail.com>
2017-08-23 15:13:40 +0200

    fixed crashed syncs

--- bin/sync.rb
+++ bin/sync.rb
@@ -1,5 +1,5 @@
 @database = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
-locs = PersonBirthDetail.find_by_sql("select distinct location_created_at AS l FROM person_birth_details").map(&:l)
+locs = PersonBirthDetail.find_by_sql("select distinct location_created_at AS l FROM person_birth_details").map(&:l) + [SETTINGS['location_id']]
 location_id = locs.collect{|l| l.to_s.rjust(5, '0').rjust(6, '1')}.join('_')
 
 source = "#{@database['protocol']}://#{@database['username']}:#{@database['password']}@#{@database['host']}:#{@database['port']}/#{@database['prefix']}_#{@database['suffix']}"
