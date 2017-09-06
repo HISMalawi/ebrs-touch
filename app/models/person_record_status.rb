10efcbf9a856f376af97810a6c17eb1d56a3952b
phundi <kennethkapundi1@gmail.com>
2017-09-06 15:39:45 +0200

    fixed crash

--- app/models/person_record_status.rb
+++ app/models/person_record_status.rb
@@ -38,7 +38,7 @@ class PersonRecordStatus < ActiveRecord::Base
       SELECT COUNT(*) c FROM person_record_statuses s
         INNER JOIN person_birth_details p ON p.person_id = s.person_id
           AND p.birth_registration_type_id IN (#{birth_type_ids.join(', ')})
-        WHERE voided = 0 AND status_id = #{status.id} GROUP BY person_id")[0]['c']
+        WHERE voided = 0 AND status_id = #{status.id} GROUP BY s.person_id")[0]['c']
     end
 
     unless approved == false
