f686378dff7350f1457fd651a4810068d2707818
phundi <kennethkapundi1@gmail.com>
2017-09-06 14:50:36 +0200

    identifier allocation queueue

--- app/models/identifier_allocation_queue.rb
+++ app/models/identifier_allocation_queue.rb
@@ -1,7 +1,6 @@
 class IdentifierAllocationQueue < ActiveRecord::Base
     self.table_name = :identifier_allocation_queue
     self.primary_key = :identifier_allocation_queue_id
-    include EbrsAttribute
 
     belongs_to :core_person, foreign_key: "person_id"
 
