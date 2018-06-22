9388a2c469ae0029a6eeca9dc0c200b77868167c
phundi <kennethkapundi1@gmail.com>
2018-06-22 13:14:38 +0200

    Fixed pers identifr

--- app/models/person_identifier.rb
+++ app/models/person_identifier.rb
@@ -1,9 +1,8 @@
 class PersonIdentifier < ActiveRecord::Base
+    include EbrsAttribute
     self.table_name = :person_identifiers
     self.primary_key = :person_identifier_id
     belongs_to :core_person, foreign_key: "person_id"
-
-    include EbrsAttribute
     belongs_to :person_identifier_types, foreign_key: "person_identifier_type_id"
 
     def self.new_identifier(person_id, type, value)
@@ -12,8 +11,7 @@ class PersonIdentifier < ActiveRecord::Base
           person_id: person_id,
           person_identifier_type_id: type_id,
           value: value,
-          voided: 0,
-          creator: User.current.id
+          voided: 0
       )
     end
 end
