fab58fc9a7047d27b8c63112b2568dfffa7ee827
phundi <kennethkapundi1@gmail.com>
2018-07-31 09:57:52 +0200

    Precompiled assets; removed freetext when ajaxURL is present?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( dashboard.js )
 Rails.application.config.assets.precompile += %w( DataTables/* )
+Rails.application.config.assets.precompile += %w( global/* )
 
 
 # Add additional assets to the asset load path
