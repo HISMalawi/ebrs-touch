b3c17f5621f39f266369b86e5209af9c0cdaa27a
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-04-11 12:44:56 +0200

    Script for adding document_id columns

---
+++ bin/add_document_id_columns.rb
@@ -0,0 +1,24 @@
+tables = ["UserRole",
+          "PersonName",
+          "PersonAttribute",
+          "BarcodeIdentifier",
+          "PersonAddress",
+          "PersonBirthDetail",
+          "PersonNameCode",
+          "PersonIdentifier",
+          "PersonRecordStatus",
+          "AuditTrail",
+          "Notification",
+          "CorePerson",
+          "PersonRelationship",
+          "Person",
+          "User"]
+
+tables.each do |table|
+
+	table_name = eval(table).table_name
+	puts "Adding document_id for table: '#{table_name}'"
+	if !eval(table).new.attributes.has_key?("document_id")
+		ActiveRecord::Base.connection.execute("ALTER TABLE #{table_name} ADD COLUMN document_id VARCHAR(255);")
+	end
+end
