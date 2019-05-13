7abb36c2f0356a9e4d8bae2a5829fec3858b5d14
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-05-13 15:39:05 +0200

    searched routes

--- config/routes.rb
+++ config/routes.rb
@@ -185,6 +185,7 @@ Rails.application.routes.draw do
   get 'person/approve_reprint_request'
   get "person/approve_amendment_request"
   get '/search' => 'dc#search'
+  get '/searched_cases' => 'person#searched_cases'
   post '/searched_cases' => 'person#searched_cases'
 
   get "/person_label" => 'person#person_label'
