38937c589e538ec958fd57cd5177c3e367cc07cb
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-05-16 08:23:30 +0200

    Added Pusher lib to

--- app/models/ebrs_attribute.rb
+++ app/models/ebrs_attribute.rb
@@ -1,17 +1,3 @@
-class Pusher <  CouchRest::Model::Base
-  configs = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
-  connection.update({
-                        :protocol => "#{configs['protocol']}",
-                        :host     => "#{configs['host']}",
-                        :port     => "#{configs['port']}",
-                        :prefix   => "#{configs['prefix']}",
-                        :suffix   => "#{configs['suffix']}",
-                        :join     => '_',
-                        :username => "#{configs['username']}",
-                        :password => "#{configs['password']}"
-                    })
-end
-
 module EbrsAttribute
 
   def send_data(hash)
--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,16 @@ require "levenshtein"
 require "zebra_printer"
 
 
+class Pusher <  CouchRest::Model::Base
+  configs = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
+  connection.update({
+                        :protocol => "#{configs['protocol']}",
+                        :host     => "#{configs['host']}",
+                        :port     => "#{configs['port']}",
+                        :prefix   => "#{configs['prefix']}",
+                        :suffix   => "#{configs['suffix']}",
+                        :join     => '_',
+                        :username => "#{configs['username']}",
+                        :password => "#{configs['password']}"
+                    })
+end
