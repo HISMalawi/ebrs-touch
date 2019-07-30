f3f695e6be719b90e42feb51d6076a9b75fa4f29
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-07-30 13:56:36 +0200

    Saving parents married field using script

--- bin/fix_parents_married.rb
+++ bin/fix_parents_married.rb
@@ -4,6 +4,9 @@ PersonBirthDetail.where("source_id LIKE '%-%' ").each.with_index do |pbd, i|
   source_id = pbd.source_id
   mass_person_id = -1 * source_id.split("#")[0].to_i
   mass_person = MassPerson.find(mass_person_id)
-
-  puts mass_person.parents_married
+  
+  if mass_person.parents_married
+	pbd.parents_married_to_each_other = 1
+	pbd.save
+  end
 end
