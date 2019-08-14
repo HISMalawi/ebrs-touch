56ab6d36314311e777e7258fe17bfb64898e21a6
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-08-14 15:44:46 +0200

    Removed File

--- bin/query_printed_records.rb
+++ bin/query_printed_records.rb
@@ -3,7 +3,18 @@ champiti  = Location.where(name: "Champiti").first
 loc_ids = Location.where(parent_location: champiti.id).map(&:location_id)
 data = {}
 
-PersonBirthDetail.find_by_sql("SELECT d.location_created_at, COUNT(*) total FROM person_birth_details d 
+loc_ids.each do |l|
+	name = Location.find(l).name
+	person_ids = PersonBirthDetail.where(location_created_at: l).pluck :person_id
+	person_ids << -1
+	print_statuses = PersonRecordStatus.where(" person_id IN (#{person_ids.join(',')}) AND status_id = #{status_id} ").select("person_id").uniq
+
+	puts "#{l} #{name}"
+	data[name] = print_statuses.count
+end
+
+=begin
+PersonBirthDetail.find_by_sql("SELECT d.location_created_at, COUNT(DISTINCT(d.person_id)) total FROM person_birth_details d 
 		 INNER JOIN person_record_statuses s ON d.person_id = s.person_id AND s.voided = 0
 		 WHERE s.status_id = #{status_id} AND (d.source_id IS NOT NULL AND d.source_id LIKE '%#%' ) 
 			AND d.location_created_at IN (#{loc_ids.join(',')}) 
@@ -11,6 +22,8 @@ PersonBirthDetail.find_by_sql("SELECT d.location_created_at, COUNT(*) total FROM
 		l = Location.find(d.location_created_at)
 		data[l.name] = d.total
 }
+=end
 
+puts data.keys.length
 puts data.values.sum
 puts data
