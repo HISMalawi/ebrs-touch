788ab37be7a7d61aa13e2ae79a69f3f16518bcac
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-08-14 09:23:22 +0200

    Added New File for Querying Printed Records

--- bin/query_printed_records.rb
+++ bin/query_printed_records.rb
@@ -2,3 +2,5 @@ status_id = Status.where(name: "DC-PRINTED").last.id
 p = PersonBirthDetail.find_by_sql("SELECT * FROM person_birth_details d 
 		 INNER JOIN person_record_statuses s ON d.person_id = s.person_id AND s.voided = 0
 		 WHERE s.status_id = #{status_id} AND (d.source_id IS NOT NULL AND d.source_id LIKE '%#%' ) ")
+
+puts "#{p.length} Records"
