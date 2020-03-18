08aa5d1f254ada4cadde1f84b99fae872797bbd6
Emmanuel Mkambankhani <emmanuel.mkambankhani@gmail.com>
2020-03-18 22:00:15 +0300

    Dispatch certificate popup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,7 +73,7 @@ class ApplicationController < ActionController::Base
   def login!(user)
     session[:user_id] = user.id
     AuditTrail.ip_address_accessor = request.remote_ip
-    AuditTrail.mac_address_accessor = (` arp #{request.remote_ip}`.split(/\n/).last.split(/\s+/)[2]  rescue MacAddress.address)
+    AuditTrail.mac_address_accessor = ((` arp #{request.remote_ip}`.split(/\n/).last.split(/\s+/)[2]  rescue MacAddress.address) rescue nil)
     AuditTrail.create(person_id: user.person_id,
                        audit_trail_type_id: AuditTrailType.find_by_name("SYSTEM").id,
                        comment: "User login") rescue nil
